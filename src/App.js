b43a81a1125279b4409961debe4c13035286f428
francopachec0 <francopacheco.dev@gmail.com>
2022-07-14 10:48:00 -0300

    installing packages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Delivery App</h1>
+    <div className="w-screen h-screen flex items-center justify-center text-gray-800">
+      <h1 className="text-3xl font-bold text-center m-2">Delivery App</h1>
     </div>
   );
 }
