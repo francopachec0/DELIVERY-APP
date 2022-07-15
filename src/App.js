311b8046ba9c9aeb43b7007cc9ae9e5ef55f185a
francopachec0 <francopacheco.dev@gmail.com>
2022-07-15 18:08:35 -0300

    abc

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center text-gray-800">
-      <h1 className="text-3xl font-bold text-center m-2">Delivery App</h1>
+    <div className="w-screen h-auto flex flex-col">
+      <Header />
     </div>
   );
 }
