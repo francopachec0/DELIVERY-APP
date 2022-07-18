86d4327081984bd7c15df41910fa7eb9b73d51b2
francopachec0 <francopacheco.dev@gmail.com>
2022-07-18 13:28:30 -0300

    git

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,20 +7,28 @@ import { Link } from "react-router-dom";
 
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { app } from "../firebase.config";
+import { useStateValue } from "../context/StateProvider";
+import { actionType } from "../context/reducer";
 
 const Header = () => {
-
   const firebaseAuth = getAuth(app);
   const provider = new GoogleAuthProvider();
 
+  const [{ user }, dispatch] = useStateValue();
+
   const login = async () => {
-    const response = await signInWithPopup(firebaseAuth , provider);
-    console.log(response);
-  }
+    const {
+      user: { refreshToken, providerData },
+    } = await signInWithPopup(firebaseAuth, provider);
+    dispatch({
+      type: actionType.SET_USER,
+      user: providerData[0],
+    });
+  };
 
   return (
     <header className="fixed z-50 w-screen p-4 px-16">
-      {/* desktop and tablet */}
+      {/* ---------------------------------- desktop and tablet --------------------------------------- */}
       <div className="hidden md:flex w-full h-full items-center justify-between">
         <Link to={"/"} className="flex items-center gap-2">
           <motion.img
@@ -66,7 +74,7 @@ const Header = () => {
           </div>
         </div>
       </div>
-      {/* mobile */}
+      {/* -------------------------------------- mobile ------------------------------------------------ */}
       <div className="flex md:hidden w-full h-full"></div>
     </header>
   );
--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
+import { StateProvider } from './context/StateProvider';
+import reducer from './context/reducer';
+import { initialState } from './context/InitialState';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StateProvider>
   </React.StrictMode>
 );
 
