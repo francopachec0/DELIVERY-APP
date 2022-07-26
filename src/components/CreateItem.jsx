77b74ca48e7e9c742ed00e00fc2dba514624e24f
francopachec0 <francopacheco.dev@gmail.com>
2022-07-25 23:48:11 -0300

    create items

--- src/components/CreateItem.jsx
+++ src/components/CreateItem.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+import { MdFastfood } from "react-icons/md";
 
 const CreateItem = () => {
-  return (
-    <div>CreateItem</div>
-  )
-}
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState(null);
+  const [category, setCategory] = useState(null);
+  const [fields, setFields] = useState(false);
+  const [alertStatus, setAlertStatus] = useState("danger");
+  const [msg, setMsg] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-export default CreateItem
+  return (
+    <div className="w-full min-h-screen flex items-center justify-center">
+      <div className="w-[90%] md:w-[75%] border border-[#023e8a] rounded-lg p-4 flex flex-col items-center justify-center">
+        {fields && (
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className={`w-full p-2 rounded-lg text-center text-lg font-semibold ${
+              alertStatus === "danger"
+                ? "bg-red-400 text-black"
+                : "bg-emerald-400 text-black"
+            }`}
+          >
+            {msg}
+          </motion.p>
+        )}
+        <div className="w-full py-2 border-b border-[#023e8a] flex items-center gap-2">
+          <MdFastfood className="text-2xl text-gray-700" />
+          <input
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+            required
+            value={name}
+            placeholder="Nombre del Producto"
+            className="w-full h-full bg-transparent font-semibold outline-none border-none text-textColor placeholder:text-gray-400"
+          />
+        </div>
+        <div className=""></div>
+      </div>
+    </div>
+  );
+};
+
+export default CreateItem;
--- src/utils/data.js
+++ src/utils/data.js
@@ -29,3 +29,42 @@ export const FoodList = [
     image: Food4,
   },
 ];
+
+export const categories = [
+  {
+    id: 1,
+    name: "Pollo",
+    urlParamName: "pollo",
+  },
+  {
+    id: 2,
+    name: "Curry",
+    urlParamName: "curry",
+  },
+  {
+    id: 3,
+    name: "Arroz",
+    urlParamName: "arroz",
+  },
+  {
+    id: 4,
+    name: "Pescado",
+    urlParamName: "pescado",
+  },
+  {
+    id: 5,
+    name: "Frutas",
+    urlParamName: "frutas",
+  },
+  {
+    id: 6,
+    name: "Helados",
+    urlParamName: "helados",
+  },
+
+  {
+    id: 7,
+    name: "Bebidas",
+    urlParamName: "bebidas",
+  },
+];
